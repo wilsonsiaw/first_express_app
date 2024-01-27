a43e6bc00087533a0c986e97027e41fe6da37a23
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-28 00:02:44 +0200

    ✨ feat(Express application): Add route handlers
    Create route handlers that will route to each individual page

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import the express module
 const express = require("express");
 
+// import the path module
+const path = require("path");
+
 // create the express application
 const app = express();
+
+// build route handlers
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"))
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "contact.html"))
+})
 
 // create the port to listen for the application
 const PORT = process.env.PORT || 5000;
